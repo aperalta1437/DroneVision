3b84f0c97499d2295c0d050c3e34e14bba392054
aperalta1437 <43322675+aperalta1437@users.noreply.github.com>
2018-12-03 10:52:38 -0500

    Add files via upload

--- new.py
+++ new.py
@@ -1,10 +1,12 @@
 from skimage import io      # Importing the io module from the scikit-image library.
-import threading
-import cv2
+import threading            # Importing the threading module to multi-thread the code.
+import cv2                  # Importing opencv-python to work on video face recognition.
 
-running = True
-frame = 0
-cam = cv2.VideoCapture(0)
+# Initializing the global variables that will be used throughout the script.
+running = True                  # running global set to true.
+frame = 0                       # frame global set to 0.
+# Creating a VideoCapture object with the default camera passed as its argument.
+cam = cv2.VideoCapture('tcp://192.168.1.1:5555')       # 'tcp://192.168.1.1:5555'
 # Creating a CascadeClassifier object to identify frontal faces.
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 
@@ -23,25 +25,37 @@ def verify_image(img_file):
 
 
 def make_720p():
-    """The make_720p function sets the resolution of the screen to 720p."""
+    """
+    The make_720p function sets the resolution of the screen to 720p.
+    """
     cam.set(3, 1280)
     cam.set(4, 720)
 
 
 def access_camera():
-    global running
-    global frame
+    """
+    The access_camera function accesses the camera to get the given frame.
+    """
+    global running      # Declaring the running global variable to be able to change it.
+    global frame        # Declaring the frame global variable to be able to change it.
+    # This while loop gets a frame per iteration of the loop.
     while True:
         running, frame = cam.read()
-        
+
 
 def save_and_show():
+    """
+    The save_and_show function saves the access_camera function's given frame, shows it and
+    recognizes the faces within the frame.
+    """
+    # This while loop saves the given frame, takes it back to show it up in the screen and executes the
+    # face recognition operations on the given frame.
     while True:
-        if running:
-            cv2.imwrite('image/now.jpg', frame)
-            img = cv2.imread('image/now.jpg', 1)
+        if running:     # If the frame was successfully read.
+            cv2.imwrite('image/now.jpg', frame)         # Saving frame into the system as an image.
+            img = cv2.imread('image/now.jpg', 1)        # Reading the image back from the system.
 
-            if verify_image('image/now.jpg'):
+            if verify_image('image/now.jpg'):       # If the image is not corrupted.
                 # Creating a copy of the imgage in grey scale.
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 # Creating a tuple with a list of detected faces within the image.
@@ -52,7 +66,7 @@ def save_and_show():
                     # Detecting one face at the time by setting each detected face equals to the first face coordinates.
                     (x, y, w, h) = faces[0]
                     # Printing the coordinates given by the face.
-                    print(x, y, w, h)
+                    # print(x, y, w, h)
 
                     color = (255, 0, 0)  # To hold the color of the rectangle that will be drawn around the face.
                     stroke = 2  # To hold the stroke of the rectangle.
@@ -66,30 +80,22 @@ def save_and_show():
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
-    cam.release()
+    cam.release()            # Releasing VideoCapture object.
     cv2.destroyAllWindows()  # Destroying all created windows.
 
 
-def show_():
-    while True:
-        img = cv2.imread('image/now.jpg', 1)
-        cv2.imshow('Frame', img)
-        cv2.waitKey(1)
-
-
 def main():
-    if __name__ == "__main__":
+    if __name__ == "__main__":      # If the current executing thread is the main function.
+        # Creating the first thread with the access_camera function as its target.
         t1 = threading.Thread(target=access_camera)
+        # Creating the second thread with the save_and_show function as its target.
         t2 = threading.Thread(target=save_and_show)
-        # t3 = threading.Thread(target=show_)
 
-        t1.start()
-        t2.start()
-        # t3.start()
+        t1.start()      # Running the first thread.
+        t2.start()      # Running the second thread.
 
-        t1.join()
-        t2.join()
-        # t3.join()
+        t1.join()       # Waiting until the first thread finishes before continuing.
+        t2.join()       # Waiting until the second thread finishes before continuing.
 
 
 main()
